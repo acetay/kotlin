071396007e81d9c5644bc1ee61ec5145fab9e6cb
ace <aceveron@gmail.com>
2023-04-11 02:13:38 +0800

    kotlin

---
+++ src/main/kotlin/com/kotlinplayground/basics/Variables.kt
@@ -0,0 +1,39 @@
+package com.kotlinplayground.basics
+
+import com.kotlinplayground.functions.courseName
+import com.kotlinplayground.functions.topLevelFunction
+
+fun main() {
+
+    val name = "Dilip"
+    println(name)
+
+    //name = "Dilip1"
+
+    var age = 34
+    println(age)
+    age = 35
+    println(age)
+
+    val salary = 30000L
+    println(salary)
+
+    val course = "Kotlin Spring"
+    println("course : $course and the course length is ${course.length}")
+
+    val multiLine = "ABC \n DEF"
+    println(multiLine)
+
+    val multiLine1 = """
+        ABC
+        DEF        
+    """.trimIndent()
+
+    println(multiLine1)
+
+    val num = topLevelFunction()
+    println("Num is : $num")
+    println("courseName : $courseName")
+
+}
+
